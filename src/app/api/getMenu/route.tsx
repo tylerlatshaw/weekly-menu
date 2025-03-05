219e246c3dd9649b91c8c3f52a8634258585cafa
Tyler Latshaw <tylerlatshaw@gmail.com>
2025-03-04 19:18:35 -0500

    Removed API route

--- src/app/api/getMenu/route.tsx
+++
@@ -1,9 +0,0 @@
-import { blob, WeeklyMenu } from "@/components/document-builder";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    return NextResponse.json(
-        { message: blob},
-        { status: 200 }
-    );
-}
